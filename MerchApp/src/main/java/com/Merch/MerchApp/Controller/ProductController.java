e65b6ec9f9d6172492e459b1d4009d879dacc53d
Anil123 <62357264+gokul678@users.noreply.github.com>
2025-02-03 00:36:59 +0530

    workitem latest commit

--- MerchApp/src/main/java/com/Merch/MerchApp/Controller/ProductController.java
+++ MerchApp/src/main/java/com/Merch/MerchApp/Controller/ProductController.java
@@ -3,9 +3,14 @@ package com.Merch.MerchApp.Controller;
 import com.Merch.MerchApp.Model.Product;
 import com.Merch.MerchApp.Service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -17,12 +22,63 @@ public class ProductController {
     private ProductService service;
 
     @GetMapping("/products")
-    public List<Product> getAllProducts(){
-        return service.getAllProducts();
+    public ResponseEntity<List<Product>> getAllProducts(){
+       List<Product> product= service.getAllProducts();
+        return new ResponseEntity<>( product, HttpStatus.OK);
     }
     @GetMapping("/product/{id}")
-    public Product getProductById(@PathVariable int id){
-        return service.getProductById(id);
+    public ResponseEntity<Product> getProductById(@PathVariable int id){
+        Product product=service.getProductById(id);
+        if (product!=null) {
+            return new ResponseEntity<>(product, HttpStatus.OK);
+        }
+        else{
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+    @PostMapping("/product")
+    public ResponseEntity<?> addProduct(@RequestPart Product product,
+                                        @RequestPart MultipartFile imageFile){
+        try {
+            Product product1=service.addProduct(product,imageFile);
+            return new ResponseEntity<>(product1,HttpStatus.CREATED);
+        } catch (Exception e) {
+           return  new ResponseEntity<>(e.getMessage(),HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+    @GetMapping("/product/{productId}/image")
+    public ResponseEntity<byte[]> getImageById(@PathVariable int productId){
+        Product product2=service.getProductById(productId);
+        if(product2==null){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        else {
+            Product product = service.getProductById(productId);
+            byte[] imageFile = product.getImageData();
+            return ResponseEntity.ok().body(imageFile);
+        }
+    }
+    @PutMapping("/product/{id}")
+    public ResponseEntity<String> updateProduct(@PathVariable int id,@RequestPart Product product,
+                                                @RequestPart MultipartFile imageFile) throws IOException {
+        Product product1=service.updateProduct(id,product,imageFile);
+        if (product1!=null){
+            return new ResponseEntity<>("Product Updated", HttpStatus.OK);
+        }
+        else {
+            return  new ResponseEntity<>("Product not updated",HttpStatus.BAD_REQUEST);
+        }
+    }
+    @DeleteMapping("/product/{id}")
+    public ResponseEntity<String> deleteProduct(@PathVariable int id){
+        Product product=service.getProductById(id);
+        if (product!=null){
+            service.deleteProduct(id);
+            return new ResponseEntity<>("Deleted",HttpStatus.OK);
+        }
+        else {
+            return new ResponseEntity<>("Not deleted",HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
 }
--- MerchApp/src/main/java/com/Merch/MerchApp/Service/ProductService.java
+++ MerchApp/src/main/java/com/Merch/MerchApp/Service/ProductService.java
@@ -3,8 +3,12 @@ package com.Merch.MerchApp.Service;
 import com.Merch.MerchApp.Model.Product;
 import com.Merch.MerchApp.Repository.ProductRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 @Service
@@ -17,6 +21,27 @@ public class ProductService {
         return repo.findAll();
     }
     public Product getProductById(int id) {
-        return repo.findById(id).get();
+        return repo.findById(id).orElse(null);
+    }
+
+    public Product addProduct(Product product, MultipartFile imageFile) throws IOException {
+        product.setImageName(imageFile.getOriginalFilename());
+        product.setImageType(imageFile.getContentType());
+        product.setImageData(imageFile.getBytes());
+        return repo.save(product);
+    }
+
+    public Product updateProduct(int id, Product product, MultipartFile imageFile) throws IOException {
+        if (id != product.getId()) {
+            new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+            product.setImageData(imageFile.getBytes());
+            product.setImageName(imageFile.getOriginalFilename());
+            product.setImageType(imageFile.getContentType());
+            return repo.save(product);
+    }
+
+    public void deleteProduct(int id) {
+        repo.deleteById(id);
     }
 }
